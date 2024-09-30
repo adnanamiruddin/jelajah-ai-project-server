eeacc6435708dcc134f2b220bdf2dd56af9b4fb4
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-10-01 02:54:08 +0800

    fix: Fix bug

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ import routes from "./src/routes/index.js";
 import "dotenv/config.js";
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://jelajah-ai-web.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/api", routes);
 
