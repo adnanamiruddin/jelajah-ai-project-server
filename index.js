2173a1cc4a13ea4e64172caec10dd400dd4ca877
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-10-01 03:01:09 +0800

    fix: Fix bug

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import routes from "./src/routes/index.js";
 import "dotenv/config.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use("/api", routes);
 
